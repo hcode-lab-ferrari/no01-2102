c5f7b1148d60373a9d3f0bea4713f2bf78b95f34
João Rangel <joaohcrangel@gmail.com>
2022-03-01 23:09:40 -0300

    nova versao

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,6 +4,6 @@ import { Controller, Get } from '@nestjs/common';
 export class AppController {
     @Get()
     getHello(): string {
-        return 'Ferrari API 3';
+        return 'Ferrari API 4';
     }
 }
